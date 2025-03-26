5c3d9f07a0fbcb92c8b2b21c93f9de8380a0af0d
arakene <arakene_dev@naver.com>
2025-03-26 21:54:33 +0900

    build: 디펜던시 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,11 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+//    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+//    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    implementation("io.appium:java-client:9.4.0")
+    implementation("org.seleniumhq.selenium:selenium-java:4.28.0")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
